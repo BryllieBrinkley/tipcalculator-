cf4435a955d61953f1ef3bb81bad3822c1640f09
Jibryll Brinkley <jibryll.s.brinkley@gmail.com>
2026-09-24 13:10:46 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jibryll Brinkley on 9/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
